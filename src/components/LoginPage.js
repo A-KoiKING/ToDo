0a0c7e9cf981c8c9e9438a21e2287fc925a3630e
A-KoiKING <yukisnowskor@gmail.com>
2025-02-25 04:46:56 +0900

    ログインページのコードを見やすく変更

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,14 +2,38 @@ import React from 'react'
 
 function LoginPage({ handleLogin, userId, setUserId, password, setPassword }) {
   return (
-    <div className="container">
-        <h1 className="title">Welcome to Hakorobo</h1>
+    <div className="loginpage">
+        <h1 className="title">
+            Welcome to Hakorobo
+        </h1>
         <form onSubmit={handleLogin} className="form">
-            <label className="login_label">ユーザーID</label>
-            <input type="text" name="userId" value={userId} onChange={(e) => setUserId(e.target.value)} required autoComplete="off" className="input"/>
-            <label className="login_label">パスワード</label>
-            <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required autoComplete="off" className="input"/>
-            <button type="submit" className="button">ログイン</button>
+            <label className="label">
+                ユーザーID
+            </label>
+            <input 
+                type="text" 
+                name="userId" 
+                value={userId} 
+                onChange={(e) => setUserId(e.target.value)} 
+                required 
+                autoComplete="off" 
+                className="input"
+            />
+            <label className="label">
+                パスワード
+            </label>
+            <input 
+                type="password" 
+                name="password" 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} 
+                required 
+                autoComplete="off" 
+                className="input"
+            />
+            <button type="submit" className="button">
+                ログイン
+            </button>
         </form>
     </div>
   )
