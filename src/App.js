1f24c70dd12e709fc9afd7ff2372a45b5ea24d00
A-KoiKING <yukisnowskor@gmail.com>
2025-02-25 01:47:22 +0900

    入力時のinputの補助を削除

--- src/App.js
+++ src/App.js
@@ -96,9 +96,9 @@ function App() {
           <h1 className="title">Welcome to Hakorobo</h1>
           <form onSubmit={handleLogin} className="form">
             <label className="login_label">ユーザーID</label>
-            <input type="text" name="userId" value={userId} onChange={(e) => setUserId(e.target.value)} placeholder="ユーザーID" required autoComplete="off" className="input"/>
+            <input type="text" name="userId" value={userId} onChange={(e) => setUserId(e.target.value)} required autoComplete="off" className="input"/>
             <label className="login_label">パスワード</label>
-            <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="パスワード" required autoComplete="off" className="input"/>
+            <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required autoComplete="off" className="input"/>
             <button type="submit" className="button">ログイン</button>
           </form>
         </div>
