8a45bb7d2292471bf19dd093faa461b24d40b691
A-KoiKING <yukisnowskor@gmail.com>
2025-02-25 01:26:04 +0900

    ログインページのUIを更新

--- src/App.js
+++ src/App.js
@@ -90,12 +90,14 @@ function App() {
   };
   
   return (
-    <div className="container">
+    <div>
       {!isLoggedIn ? (
-        <div>
-          <h1>Welcome Page</h1>
+        <div className="container">
+          <h1 className="title">Welcome to Hakorobo</h1>
           <form onSubmit={handleLogin} className="form">
+            <label className="login_label">ユーザーID</label>
             <input type="text" name="userId" value={userId} onChange={(e) => setUserId(e.target.value)} placeholder="ユーザーID" required autoComplete="off" className="input"/>
+            <label className="login_label">パスワード</label>
             <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="パスワード" required autoComplete="off" className="input"/>
             <button type="submit" className="button">ログイン</button>
           </form>
