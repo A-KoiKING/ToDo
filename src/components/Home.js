a996a5f6adc38fb4ef8c3ccf5983ff82a3f3c52d
A-KoiKING <yukisnowskor@gmail.com>
2025-03-03 23:24:47 +0900

    スマホでスクロールできてしまうバグを修正

--- src/components/Home.js
+++ src/components/Home.js
@@ -78,8 +78,8 @@ function Home({ lastUpdated, version, notice, targetDate }) {
         </p>
       )}
       <p><strong>お知らせ:</strong> {notice || 'お知らせはありません'}</p>
-      <p><strong>最終更新日:</strong> {lastUpdated || '未設定'}</p>
       <p><strong>バージョン:</strong> {version || '未設定'}</p>
+      <p><strong>最終更新日:</strong> {lastUpdated || '未設定'}</p>
     </div>
   );
 }
